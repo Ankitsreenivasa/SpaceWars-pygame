eb35895abd74e23c15edb9aafe03b18316409c40
Ankitsreenivasa <ankit.sreeni@gmail.com>
2023-08-31 23:39:51 +0530

    second commit

--- Students/students/home/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Student
-
-
-class StudentsSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Student
-        fields = ('id', 'name', 'college', 'address', 'aadhar')
--- Students/students/home/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import include, path
-from .views import StudentsView
-
-urlpatterns = [
-    path('', StudentsView.as_view(
-        {'get': 'list', 'post': 'create'}), name='students_list'),
-]
